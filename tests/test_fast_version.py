583f5cef4430f872123e2ff6c8f1949b328a3725
Waket Zheng <waketzheng@gmail.com>
2026-03-25 22:29:30 +0800

    feat: expose fast command in script section

--- tests/test_fast_version.py
+++ tests/test_fast_version.py
@@ -1,12 +1,13 @@
-import subprocess
 import shlex
+import subprocess
 
 
 def capture_output(cmd: str) -> str:
-    r = subprocess.run(shlex.split(cmd), capture_output=True, text=True, encoding='utf-8')
+    r = subprocess.run(shlex.split(cmd), capture_output=True, text=True, encoding="utf-8")
+    return r.stdout.strip()
 
 
 def test_uvx():
-    cmd_old = 'uvx --from fastdevcli-slim fast version'
-    cmd_new = 'uvx --from fast-dev-cli fast version'
+    cmd_old = "uvx --from fastdevcli-slim fast version"
+    cmd_new = "uvx --from fast-dev-cli fast version"
     assert capture_output(cmd_old) == capture_output(cmd_new)
